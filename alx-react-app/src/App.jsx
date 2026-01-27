7e3d2807096d4044d3bbfcbf349409b5a5482173
Christian C A George <christiancageorge@gmail.com>
2026-01-27 19:50:00 +0100

    Add React components: Header, MainContent, Footer, UserProfile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import './App.css';
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   return (
     <div>
       <Header />
       <MainContent />
+
+      {/* Example usage of UserProfile */}
+      <UserProfile 
+        name="Christian George" 
+        age={25} 
+        bio="Loves hiking and photography" 
+      />
+      <UserProfile 
+        name="Bob" 
+        age={30} 
+        bio="Enjoys cooking and traveling" 
+      />
+
       <Footer />
     </div>
   );
