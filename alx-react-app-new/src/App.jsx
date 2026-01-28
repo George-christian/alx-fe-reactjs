57584a9f8cdbf7c196a47f262d717b28fa10fca7
Christian C A George <christiancageorge@gmail.com>
2026-01-28 09:47:28 +0100

    Added Counter components using useState

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,28 +1,18 @@
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
-import Footer from "./components/Footer";
-import UserProfile from "./components/UserProfile";
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import UserProfile from './components/UserProfile';
+import Footer from './components/Footer';
+import Counter from './components/Counter';
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
       <MainContent />
-
-      {/* Example usage of UserProfile */}
-      <UserProfile 
-        name="Christian George" 
-        age={25} 
-        bio="Loves hiking and photography" 
-      />
-      <UserProfile 
-        name="Bob" 
-        age={30} 
-        bio="Enjoys cooking and traveling" 
-      />
-
+      <UserProfile name="Christian George" age={25} bio="A React enthusiast" />
+      <Counter />
       <Footer />
-    </div>
+    </>
   );
 }
 
