2fbd26943e7b88b42c6f24dc83088ec56ec78445
Christian C A George <christiancageorge@gmail.com>
2026-02-14 17:31:26 +0100

    Add Home Page with recipe cards using React and Tailwind CSS

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,11 +1,8 @@
+import React from "react";
+import HomePage from "./components/HomePage";
+
 function App() {
-  return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-500">
-        🍲 Recipe Sharing Platform
-      </h1>
-    </div>
-  )
+  return <HomePage />;
 }
 
-export default App
+export default App;
