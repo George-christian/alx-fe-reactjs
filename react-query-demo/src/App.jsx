be4097fd68005d3225a00f768dc58f26c93c91b3
Christian C A George <christiancageorge@gmail.com>
2026-02-18 14:55:03 +0100

    Add react-query-demo: fetch posts using React Query with caching and refetch

---
+++ react-query-demo/src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from "react";
+import PostsComponent from "./components/PostsComponent.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Query Demo</h1>
+      <PostsComponent />
+    </div>
+  );
+}
+
+export default App;
